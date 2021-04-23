d8bbc35dca2ea1a94c49afbd7c29c8613761e60f
sdannyzaidi <65749181+sdannyzaidi@users.noreply.github.com>
2021-04-23 20:59:29 +0500

    minor changes

--- lib/pages/viewResolve.dart
+++ lib/pages/viewResolve.dart
@@ -4,7 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:lums_student_portal/Backend/validators.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:lums_student_portal/themes/progessIndicator.dart';
+import 'package:lums_student_portal/Themes/progessIndicator.dart';
 
 class ViewResolve extends StatefulWidget {
   late final String subject;
